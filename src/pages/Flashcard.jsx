883053cb9da20d533640347c2d87e9daca60a054
abhishekkrawat <abhishekrws01@gmail.com>
2023-07-17 09:57:10 +0100

    add chevron-left and chevron-right iconbuttons

--- src/pages/Flashcard.jsx
+++ src/pages/Flashcard.jsx
@@ -5,42 +5,108 @@ import {
   Divider,
   Grid,
   GridItem,
+  IconButton,
   SimpleGrid,
   Stack,
   Text,
   VStack,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { ChevronLeft, ChevronRight } from 'react-feather';
 import Navbar from '../components/NavigationBar/Navbar';
 
 export const Flashcard = () => {
   return (
     <>
       <Navbar />
-      <Container as='section' maxW='9xl' py='40px'>
-        <Grid templateColumns='repeat(3, 1fr)'>
-          <GridItem colStart={1}>
-            <VStack p={3} spacing={8} align='stretch'>
-              <Box w='200px' h='100px' display={'flex'} flexDirection={'column'} bg={'purple.100'}>
+      <Container
+        as='section'
+        maxW='9xl'
+        py='40px'
+        h={'92vh'}
+        bg={useColorModeValue('gray.50', 'gray.800')}
+      >
+        <Grid h={10} templateColumns='repeat(3, 1fr)'>
+          <GridItem
+            overflow='scroll'
+            overflowX='hidden'
+            rowSpan={4}
+            colSpan={2}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+            borderRadius={15}
+          >
+            <VStack p={2} spacing={4} align='stretch'>
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
                 <Text align='center'>1/18</Text>
               </Box>
               <Divider />
-              <Box w='200px' h='100px' display={'flex'} flexDirection={'column'} bg={'purple.100'}>
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
                 <Text align='center'>2/18</Text>
               </Box>
               <Divider />
-              <Box w='200px' h='100px' display={'flex'} flexDirection={'column'} bg={'purple.100'}>
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
                 <Text align='center'>3/18</Text>
               </Box>
               <Divider />
-              <Box w='200px' h='100px' display={'flex'} flexDirection={'column'} bg={'purple.100'}>
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
                 <Text align='center'>4/18</Text>
+              </Box>
+              <Divider />
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
+                <Text align='center'>5/18</Text>
+              </Box>
+              <Divider />
+              <Box
+                w='175px'
+                h='110px'
+                display={'flex'}
+                flexDirection={'column'}
+                bg={'purple.100'}
+                borderRadius={15}
+              >
+                <Text align='center'>6/18</Text>
               </Box>
             </VStack>
           </GridItem>
-          <GridItem colSpan={1}>
-            <SimpleGrid display={'flex'} alignContent={'center'} justifyContent={'center'}>
-              <Center py={100} px={400}>
+          <GridItem bg={useColorModeValue('gray.50', 'gray.800')}>
+            <SimpleGrid>
+              <Center py={100} px={290} gap={10}>
+                <IconButton w={20} h={200} icon={<ChevronLeft size={'md'}/>} bg={useColorModeValue('gray.50', 'gray.800')} />
                 <Stack
                   borderWidth='2px'
                   borderRadius='xl'
@@ -48,8 +114,9 @@ export const Flashcard = () => {
                   w={{ sm: '100%', md: '630px' }}
                   height={{ sm: '476px', md: '25rem' }}
                   bg={useColorModeValue('white', 'purple.900')}
-                  boxShadow={'2xl'}
+                  boxShadow={'1xl'}
                 ></Stack>
+                <IconButton w={20} h={200} icon={<ChevronRight size={'md'}/>} bg={useColorModeValue('gray.50', 'gray.800')} />
               </Center>
             </SimpleGrid>
           </GridItem>
