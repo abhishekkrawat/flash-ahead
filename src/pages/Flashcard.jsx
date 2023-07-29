c9b4f9eb1b50ca5b1d24ae673a34b1877f839110
abhishekkrawat <abhishekrws01@gmail.com>
2023-07-29 20:37:21 +0100

    Use a flexbox instead of a grid

--- src/pages/Flashcard.jsx
+++ src/pages/Flashcard.jsx
@@ -1,14 +1,12 @@
 import {
   Box,
-  Container,
   Divider,
-  Grid,
-  GridItem,
   // IconButton,
-  Stack,
+  Flex,
   Text,
   VStack,
   useColorModeValue,
+  Center,
 } from '@chakra-ui/react';
 // import { ChevronLeft, ChevronRight } from 'react-feather';
 import Navbar from '../components/NavigationBar/Navbar';
@@ -17,115 +15,133 @@ export const Flashcard = () => {
   return (
     <>
       <Navbar />
-      <Container
-        as='section'
-        maxW='9xl'
-        py={{ base: '20px', md: '40px' }}
-        minH='92vh'
-        bg={useColorModeValue('gray.50', 'gray.800')}
-      >
-        <Grid h={10} templateColumns={'repeat(5, 1fr)'} justifyItems={'start'} columnGap={550}>
-          <GridItem
-            overflow='scroll'
-            overflowX='hidden'
-            bg={useColorModeValue('gray.50', 'gray.800')}
+      <Flex minH='100vh'>
+        <VStack
+          padding={3}
+          gap={1}
+          display={'flex'}
+          justifyContent={'space-between'}
+          bg={'gray.100'}
+          overflow={'scroll'}
+          overflowX={'hidden'}
+        >
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg='white'
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
           >
-            <VStack padding={2} spacing={4}>
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg='white'
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>1/18</Text>
-              </Box>
-              <Divider />
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg={'white'}
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>2/18</Text>
-              </Box>
-              <Divider />
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg={'white'}
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>3/18</Text>
-              </Box>
-              <Divider />
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg={'white'}
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>4/18</Text>
-              </Box>
-              <Divider />
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg={'white'}
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>5/18</Text>
-              </Box>
-              <Divider />
-              <Box
-                w={{ base: '50px', md: '175px' }}
-                h='110px'
-                bg={'white'}
-                borderRadius='10px'
-                borderWidth='2px'
-                borderColor='purple.400'
-              >
-                <Text align='center'>6/18</Text>
-              </Box>
-            </VStack>
-          </GridItem>
+            <Text align='center'>1/18</Text>
+          </Box>
+          <Divider />
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg={'white'}
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
+          >
+            <Text align='center'>2/18</Text>
+          </Box>
+          <Divider />
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg={'white'}
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
+          >
+            <Text align='center'>3/18</Text>
+          </Box>
+          <Divider />
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg={'white'}
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
+          >
+            <Text align='center'>4/18</Text>
+          </Box>
+          <Divider />
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg={'white'}
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
+          >
+            <Text align='center'>5/18</Text>
+          </Box>
+          <Divider />
+          <Box
+            w={{ base: '50px', md: '175px' }}
+            h='110px'
+            bg={'white'}
+            borderRadius='10px'
+            borderWidth='2px'
+            borderColor='purple.400'
+          >
+            <Text align='center'>6/18</Text>
+          </Box>
+        </VStack>
+        <Box
+          display={'flex'}
+          flex={1}
+          alignItems={'center'}
+          justifyContent={'center'}
+          bg={'gray.100'}
+        >
+          <Center h={100}>
+            <Box
+              minW={600}
+              minH={400}
+              maxW={'lg'}
+              w={'100%'}
+              h={'100%'}
+              bg={useColorModeValue('white', 'purple.900')}
+              borderWidth={'3px'}
+              borderRadius={'xl'}
+              borderColor={'purple.400'}
+              boxShadow={'2xl'}
+            >
+              <Text>1/18</Text>
+            </Box>
+          </Center>
+        </Box>
 
-          <GridItem paddingY={100} gap={2} justifyContent={'center'}>
-            {/* <IconButton
+        {/* <GridItem paddingY={100} gap={2} justifyContent={'center'}> 
+           <IconButton
               w={{ base: '10px', md: '20px' }}
               h={{ base: '100px', md: '200px' }}
               icon={<ChevronLeft size={'md'} />}
               bg={useColorModeValue('gray.50', 'gray.800')}
             /> */}
-            <Stack
-              borderWidth='3px'
-              borderRadius='xl'
-              borderColor={'purple.400'}
-              w={{ sm: '315px', md: '630px' }}
-              height={{ sm: '15em', md: '25em' }}
-              bg={useColorModeValue('white', 'purple.900')}
-              boxShadow={'2xl'}
-            >
-              <Text align='center'>1/18</Text>
-            </Stack>
-            {/* <IconButton
+        {/* <Stack 
+            borderWidth='3px'
+            borderRadius='xl'
+            borderColor={'purple.400'}
+            w={{ sm: '315px', md: '630px' }}
+            height={{ sm: '15em', md: '25em' }}
+            bg={useColorModeValue('white', 'purple.900')}
+            boxShadow={'2xl'}
+          >
+            <Text align='center'>1/18</Text>
+          </Stack> */}
+        {/* <IconButton
               w={{ base: '10px', md: '20px' }}
               h={{ base: '100px', md: '200px' }}
               icon={<ChevronRight size={'md'} />}
               bg={useColorModeValue('gray.50', 'gray.800')}
-            /> */}
-          </GridItem>
-        </Grid>
-      </Container>
+            /> 
+         </GridItem> */}
+      </Flex>
     </>
   );
 };
