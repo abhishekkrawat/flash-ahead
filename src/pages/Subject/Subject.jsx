ce30e94bd08d08906943dde3605fcd6c0541ce1c
abhishekkrawat <abhishekrws01@gmail.com>
2023-08-04 19:52:09 +0100

    add filtering logic

--- src/pages/Subject/Subject.jsx
+++ src/pages/Subject/Subject.jsx
@@ -4,28 +4,51 @@ import { SidePanel } from './SidePanel';
 import { Decks } from './Decks';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../supabaseClient';
-import { transform } from '../../utils';
-import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const Subject = () => {
-  const [decks, setDecks] = useState([]);
+  const [topics, setTopics] = useState([]);
   const [qualifications, setQualifications] = useState([]);
   const [boards, setBoards] = useState([]);
   const [subjects, setSubjects] = useState([]);
-  const { subjectName } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const getDecks = async () => {
-    const { data, error } = await supabase.rpc('get_topics', {
-      subjectname: transform(subjectName.trim()),
+  const filterTopics = (topics) => {
+    return topics.filter((topic) => {
+      if (searchParams.get('s')) {
+        const selectedSubjects = searchParams.get('s').split('_');
+        const getSubjectName = (id) => {
+          return subjects.find((s) => s.subject_id === id).subject_name;
+        };
+        return selectedSubjects.includes(getSubjectName(topic.subject_id));
+      } else if (searchParams.get('q')) {
+        const selectedQualifications = searchParams.get('q').split('_');
+        const getQualificationName = (id) => {
+          return qualifications.find((q) => q.qualification_id === id).qualification_name;
+        };
+        return selectedQualifications.includes(getQualificationName(topic.qualification_id));
+      } else if (searchParams.get('b')) {
+        const selectedBoards = searchParams.get('b').split('_');
+        const getBoardName = (id) => {
+          return boards.find((q) => q.board_id === id).board_name;
+        };
+        return selectedBoards.includes(getBoardName(topic.board_id));
+      }
+      return true;
     });
+  };
+
+  const getTopics = async () => {
+    const { data, error } = await supabase.from('topic').select();
+
     if (error) {
       throw new Error(error);
     }
-    setDecks(data);
+    setTopics(filterTopics(data));
   };
 
   const getSubjects = async () => {
-    const { data, error } = await supabase.from('subject').select('*');
+    const { data, error } = await supabase.from('subject').select();
 
     if (error) {
       throw new Error(error);
@@ -52,18 +75,23 @@ const Subject = () => {
   };
 
   useEffect(() => {
-    getDecks();
+    setSearchParams();
     getBoards();
     getSubjects();
     getQualifications();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    getTopics();
+  }, [searchParams]);
+
   return (
     <>
       <Navbar />
       <Container as='section' maxW='8xl' py='50px'>
         <Grid templateColumns='repeat(4, 1fr)'>
           <SidePanel qualifications={qualifications} boards={boards} subjects={subjects} />
-          <Decks decks={decks} />
+          <Decks decks={topics} />
         </Grid>
       </Container>
     </>
