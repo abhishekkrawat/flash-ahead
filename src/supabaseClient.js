f302f344bcc29bd0d43d90628559c0af94adc0a8
abhishekkrawat <abhishekrws01@gmail.com>
2023-09-16 17:45:48 +0100

    update supabaseUrl and supabaseAnonKey

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://qalrwrjexvconhlgbzxq.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhbHJ3cmpleHZjb25obGdienhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUyOTkxOTMsImV4cCI6MjAwMDg3NTE5M30.ADsw8ga3NxEM7ZqPNftH-zEh7qEdePKmNJ9namEjt_w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
