c73433621ebfee0e26aa9c33a11667be7f659147
abhishekkrawat <abhishekrws01@gmail.com>
2023-09-16 20:00:27 +0100

    basic dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,110 +2,75 @@ import {
   Heading,
   Avatar,
   Box,
-  Center,
   Text,
   Stack,
   Button,
-
-  Badge,
   useColorModeValue,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import Navbar from '../components/NavigationBar/Navbar';
 
 export const Dashboard = () => {
   return (
-    <Center py={6}>
-      <Box
-        maxW={'320px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        p={6}
-        textAlign={'center'}>
-        <Avatar
-          size={'xl'}
-          src={'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg'}
-          mb={4}
-          pos={'relative'}
-          _after={{
-            content: '""',
-            w: 4,
-            h: 4,
-            bg: 'green.300',
-            border: '2px solid white',
-            rounded: 'full',
-            pos: 'absolute',
-            bottom: 0,
-            right: 3,
-          }}
-        />
-        <Heading fontSize={'2xl'} fontFamily={'body'}>
-          Abhishek Rawat
-        </Heading>
-        <Text fontWeight={600} color={'gray.500'} mb={4}>
-          @abhishekrawat
-        </Text>
-        <Text
+    <>
+      <Navbar />
+      <Text fontSize={'x-large'} marginTop={25} marginLeft={2}>
+        Welcome, Abhishek
+      </Text>
+      <Box py={6} marginLeft={2}>
+        <Box
+          maxW={'320px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.900')}
+          boxShadow={'2xl'}
+          rounded={'lg'}
+          p={6}
           textAlign={'center'}
-          color={useColorModeValue('gray.700', 'gray.400')}
-          px={3}>
-          Actress, musician, songwriter and artist. PM for work inquires or{' '}
-          <Text color={'blue.400'}>#tag</Text> me in your posts
-        </Text>
-
-        <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}>
-            #art
-          </Badge>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}>
-            #photography
-          </Badge>
-          <Badge
-            px={2}
-            py={1}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-            fontWeight={'400'}>
-            #music
-          </Badge>
-        </Stack>
-
-        <Stack mt={8} direction={'row'} spacing={4}>
-          <Button
-            flex={1}
-            fontSize={'sm'}
-            rounded={'full'}
-            _focus={{
-              bg: 'gray.200',
-            }}>
-            Message
-          </Button>
-          <Button
-            flex={1}
-            fontSize={'sm'}
-            rounded={'full'}
-            bg={'blue.400'}
-            color={'white'}
-            boxShadow={
-              '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-            }
-            _hover={{
-              bg: 'blue.500',
+        >
+          <Avatar
+            size={'xl'}
+            src={'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg'}
+            mb={4}
+            pos={'relative'}
+            _after={{
+              content: '""',
+              w: 4,
+              h: 4,
+              bg: 'green.300',
+              border: '2px solid white',
+              rounded: 'full',
+              pos: 'absolute',
+              bottom: 0,
+              right: 3,
             }}
-            _focus={{
-              bg: 'blue.500',
-            }}>
-            Follow
-          </Button>
-        </Stack>
+          />
+          <Heading fontSize={'2xl'} fontFamily={'body'}>
+            Abhishek Rawat
+          </Heading>
+          <Stack mt={8} direction={'row'} spacing={4}>
+            <Button
+              flex={1}
+              fontSize={'sm'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500',
+              }}
+              _focus={{
+                bg: 'blue.500',
+              }}
+            >
+              Follow
+            </Button>
+          </Stack>
+        </Box>
       </Box>
-    </Center>
-  )
-}
+      <Text fontSize={'x-large'} marginLeft={2} marginTop={5}>
+        Recently added
+      </Text>
+    </>
+  );
+};
