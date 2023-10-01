25d8e12d6a5a58df50dd0504f1cb619c32c23067
abhishekkrawat <abhishekrws01@gmail.com>
2023-10-01 17:07:25 +0100

    change in page layout - 1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,26 @@
 import { IconButton } from '@chakra-ui/button';
 import Navbar from '../components/NavigationBar/Navbar';
 import { Plus } from 'react-feather';
+import { Text, Flex, Box, GridItem } from '@chakra-ui/react';
 
 export const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <IconButton display={'flex'} minW={'xs'} minH={'xs'} marginLeft={'10'} marginTop={'32'}>
-        <Plus size={'60px'} />
-      </IconButton>
+      <Flex direction={'column'} marginLeft={'10'} marginTop={'20'} p={2} gap={'20'}>
+        <Box gap={2}>
+          <Text fontSize={'2xl'} fontFamily={'cursive'}>
+            Hi, Abhishek Rawat
+          </Text>
+          <IconButton minW={'xs'} minH={'xs'}>
+            <Plus size={'60px'} />
+          </IconButton>
+        </Box>
+        <Box>
+          <Text fontSize={'2xl'}>Recently added</Text>
+          <GridItem rowSpan={2} colSpan={1}></GridItem>
+        </Box>
+      </Flex>
     </>
   );
 };
