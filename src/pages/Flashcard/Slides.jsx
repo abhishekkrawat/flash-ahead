0031bd30a42eec118f89f0476623a7319555b874
abhishekkrawat <abhishekrws01@gmail.com>
2023-10-08 19:22:04 +0100

    remove unnecessary part of code

--- src/pages/Flashcard/Slides.jsx
+++ src/pages/Flashcard/Slides.jsx
@@ -10,7 +10,6 @@ import { Divider, Text, SimpleGrid, GridItem } from '@chakra-ui/react';
 export const Slides = ({ onSelected, flashcards, selected }) => {
   return (
     <SimpleGrid
-      key={flashcards}
       h={'100vh'}
       overflow={'scroll'}
       // to hide scrollbar
