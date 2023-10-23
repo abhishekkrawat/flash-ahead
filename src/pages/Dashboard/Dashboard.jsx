2b7e5b5a3ef034456e5a6f5fabe1cdc4ae5924c1
abhishekkrawat <abhishekrws01@gmail.com>
2023-10-23 15:12:07 +0100

    add card component in dashboard jsx file

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,174 @@
-import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Divider,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Stack,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { Flex, Spacer } from '@chakra-ui/react';
 import { DashCard } from './DashCard';
-import { NewCard } from '../../components/NewCard';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
+import { Plus } from 'react-feather';
 
 export const Dashboard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const OverlayOne = () => (
+    <ModalOverlay bg='blackAlpha.300' backdropFilter='blur(10px) hue-rotate(90deg)' />
+  );
+
+  const [overlay, setOverlay] = useState(<OverlayOne />);
   return (
     <>
       <Container maxW='container.lg' marginTop={16}>
         <Flex alignItems={'center'} justifyContent={'center'}>
           <Heading>Dashboard</Heading>
-
           <Spacer />
+          <IconButton
+            aria-label='Create Deck'
+            pos={'fixed'}
+            right={0}
+            bottom={0}
+            margin={8}
+            variant={'solid'}
+            bg={'purple.300'}
+            color={'white'}
+            rounded={'full'}
+            width={14}
+            height={14}
+            boxShadow={'xl'}
+            onClick={() => {
+              setOverlay(<OverlayOne />);
 
-          {/* <Button leftIcon={<Plus />} colorScheme='purple'>
-            Add new
-          </Button> */}
-          <NewCard />
+              onOpen();
+            }}
+            _hover={{
+              bg: 'purple.500',
+
+              transform: 'scale(1.05)',
+            }}
+            icon={<Plus width={'2rem'} height={'2rem'} />}
+          />
+          <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            {overlay}
+
+            <ModalContent minW={700} h={700}>
+              <ModalHeader>New deckcard</ModalHeader>
+
+              <ModalCloseButton />
+
+              <ModalBody>
+                <Formik
+                  initialValues={{
+                    subject: '',
+                    topic: '',
+                    board: '',
+                    flashcardFront: '',
+                    flashcardBack: '',
+                  }}
+                >
+                  {() => (
+                    <Form>
+                      <Stack spacing={4}>
+                        <Stack spacing={4} direction={'row'}>
+                          <Field name='subject'>
+                            {({ field }) => (
+                              <FormControl isRequired>
+                                <FormLabel>Subject:</FormLabel>
+
+                                <Input type='text' {...field} />
+                              </FormControl>
+                            )}
+                          </Field>
+
+                          <Field name='topic'>
+                            {({ field, form }) => (
+                              <FormControl
+                                isInvalid={form.errors.password && form.touched.password}
+                                isRequired
+                              >
+                                <FormLabel>Topic:</FormLabel>
+
+                                <Input type='text' {...field} />
+                              </FormControl>
+                            )}
+                          </Field>
+
+                          <Field name='board'>
+                            {({ field, form }) => (
+                              <FormControl
+                                isInvalid={form.errors.password && form.touched.password}
+                                isRequired
+                              >
+                                <FormLabel>Board:</FormLabel>
+
+                                <Input type='text' {...field} />
+                              </FormControl>
+                            )}
+                          </Field>
+                        </Stack>
+                        <Spacer />
+                        <Text fontWeight={'bold'}>Flashcard 1:</Text>
+                        <Field name='flashcardFront'>
+                          {({ field, form }) => (
+                            <FormControl
+                              isInvalid={form.errors.password && form.touched.password}
+                              isRequired
+                            >
+                              <FormLabel>Flashacard front:</FormLabel>
+
+                              <Input type='text' {...field} />
+                            </FormControl>
+                          )}
+                        </Field>
+                        <Field name='flashcardBack'>
+                          {({ field, form }) => (
+                            <FormControl
+                              isInvalid={form.errors.password && form.touched.password}
+                              isRequired
+                            >
+                              <FormLabel>Flashacard back:</FormLabel>
+
+                              <Input type='text' {...field} />
+                            </FormControl>
+                          )}
+                        </Field>
+
+                        <Stack spacing={5}>
+                          <Button
+                            type='submit'
+                            bg={'purple.400'}
+                            color={'white'}
+                            _hover={{
+                              bg: 'purple.700',
+                            }}
+                            mb={3}
+                            isDisabled={false}
+                          >
+                            Save
+                          </Button>
+                        </Stack>
+                      </Stack>
+                    </Form>
+                  )}
+                </Formik>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
         </Flex>
         <Divider marginTop={2} marginBottom={2} />
         <SimpleGrid columns={2} spacing={5} pt={6}>
